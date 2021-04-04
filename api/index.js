08acb221a5f7b9cc141c6709e0ae9f956deaf80a
IanMarcony <ianmarconysalazar@outlook.com>
2021-04-03 21:24:58 -0400

    Updated project with  live reload

--- api/index.js
+++ api/index.js
@@ -1,25 +1,44 @@
 const express = require("express");
-const app = express();
-const nunjucks = require("nunjucks");
+const app = require("express")();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const cors = require("cors");
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
+const nunjucks = require("nunjucks");
 nunjucks.configure("views", {
   express: app,
   noCache: true,
 });
 
-app.get("/", (req, res) => {
+app.use("/", (req, res) => {
+  return res.render("home.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("Socket conectado: " + socket.id);
   const { registers } = require("../data.json");
   const sum = registers.reduce(
     (total, register) => total + register.velocidade,
     0
   );
   const media = sum / registers.length;
-  return res.render("home.html", { registers, media });
+  socket.emit("previous", { registers, media });
+  socket.on("message", (data) => {
+    console.log(data);
+    const { registers } = require("../data.json");
+    const sum = registers.reduce(
+      (total, register) => total + register.velocidade,
+      0
+    );
+    const media = sum / registers.length;
+    socket.emit("updated", { registers, media });
+  });
 });
 
-app.listen(5555, () =>
+server.listen(5555, () =>
   console.log("Server is running on http://localhost:5555")
 );
--- api/public/script.js
+++ api/public/script.js
@@ -1,5 +1,32 @@
+var socket = io();
+
 $(document).ready(function () {
+  function renderRegisters(registers) {
+    $(".registers").html("");
+    for (let index = registers.length - 1; index >= 0; index--) {
+      const element = registers[index];
+
+      $(".registers").append(`<li><h1>${element.data_hora}</h1>
+      <p>Velocidade: ${element.velocidade.toFixed(2)} mbps</p></li>`);
+    }
+  }
+
+  socket.on("updated", (data) => {
+    console.log(data);
+    $(".media").html("");
+    $(".media").append(`Média: ${data.media.toFixed(2)} mbps`);
+    renderRegisters(data.registers);
+  });
+
+  socket.on("previous", (data) => {
+    console.log(data);
+    $(".media").html("");
+    $(".media").append(`Média: ${data.media.toFixed(2)} mbps`);
+    renderRegisters(data.registers);
+  });
+
   $("button").click(function () {
-    location.reload(true);
+    console.log("clicou");
+    socket.emit("message", { reload: true });
   });
 });
